3ef50d99852d3cf9f0e8cf804606ea4eac3a3d64
Петрунина Оксана <ovpetrunina@gmail.com>
2022-07-14 19:05:30 +0300

    Test file добавлен (нерабочий)

---
+++ test/lesson2/AbstractrShapeTest.java
@@ -0,0 +1,26 @@
+package lesson2;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractrShapeTest {
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @Test
+    void getPerimeterLength() {
+    }
+
+    @Test
+    void getAreaSize() {
+    }
+
+    @Test
+    void getType() {
+    }
+}
